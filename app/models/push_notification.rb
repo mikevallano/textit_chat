6698c44c0810b6d00408250929e90745ebbf5399
Marvin Arnold <marvinmarnold@gmail.com>
2014-12-14 16:30:48 +0100

    Push notifications working

--- app/models/push_notification.rb
+++ app/models/push_notification.rb
@@ -1,13 +1,15 @@
 class PushNotification < ActiveRecord::Base
   def self.push
+    messages_sent = {}
     clients = Client.where(general_information_requested: true)
 
     all.each do |n|
       clients.each do |c|
-        unless c.push_notifications.where(id: n.id)
-          m = new_push_notification(c.phone_number, n.message)
+        if c.push_notifications.where(id: n.id).blank? && !messages_sent[c.id]
+          messages_sent[c.id] = true
+          m = Message.new_push_notification(c.phone_number, n.message)
           if m.save
-            SentPushNotification.create(client: c, push_notification: n)
+            SentPushNotification.create!(client: c, push_notification: n)
           end
         end
       end
--- test/models/push_notification_test.rb
+++ test/models/push_notification_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class PushNotificationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "sends push notificaitons to client" do
+    c1 = Client.create!(phone_number: rand_s, general_information_requested: true)
+    PushNotification.push
+
+    assert Chat.where(client_id: c1.id).first.messages.size == 1
+
+    c2 = Client.create!(phone_number: rand_s, general_information_requested: true)
+    PushNotification.push
+
+    assert Chat.where(client: c1).first.messages.size == 2
+    assert Chat.where(client: c2).first.messages.size == 1
+  end
+
+  test "does not send push notifications to unsubscribed client" do
+    c1 = Client.create!(phone_number: rand_s)
+    PushNotification.push
+
+    assert Chat.where(client: c1).empty?
+  end
+
+  def rand_s
+    (0...8).map { (65 + rand(26)).chr }.join
+  end
+
 end
